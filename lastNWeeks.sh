3dcafd6c3e3087345ebac195fe470b06721c5607
Stanislaw Baranski <stan@stan.bar>
2022-08-12 01:53:01 +0200

    add psychology calendar

--- lastNWeeks.sh
+++ lastNWeeks.sh
@@ -2,27 +2,26 @@
 LAST_N_WEEKS=${1:-1}
 
 # PhD
-venv/bin/python ./cli.py 1 8 $LAST_N_WEEKS "" 1
+venv/bin/python ./cli.py 9 8 $LAST_N_WEEKS "" 1
 cp out/phd/csv/latest.csv out/phd.csv
 # stasbar
-venv/bin/python ./cli.py 2 8 $LAST_N_WEEKS "" 1
+venv/bin/python ./cli.py 15 8 $LAST_N_WEEKS "" 1
 cp out/stasbar-sp-z-o-o/csv/latest.csv out/stasbar.csv
 # Dancing
-venv/bin/python ./cli.py 16 8 $LAST_N_WEEKS "" 1
+venv/bin/python ./cli.py 4 8 $LAST_N_WEEKS "" 1
 cp out/dancing/csv/latest.csv out/dancing.csv
 # Friends
-venv/bin/python ./cli.py 15 8 $LAST_N_WEEKS "" 1
+venv/bin/python ./cli.py 7 8 $LAST_N_WEEKS "" 1
 cp out/friends/csv/latest.csv out/friends.csv
 # Self-investement
-venv/bin/python ./cli.py 12 8 $LAST_N_WEEKS "" 1
+venv/bin/python ./cli.py 8 8 $LAST_N_WEEKS "" 1
 cp out/self-investment/csv/latest.csv out/self-investment.csv
 # Health
-venv/bin/python ./cli.py 14 8 $LAST_N_WEEKS "" 1
+venv/bin/python ./cli.py 3 8 $LAST_N_WEEKS "" 1
 cp out/health/csv/latest.csv out/health.csv
 # Flow
-venv/bin/python ./cli.py 17 8 $LAST_N_WEEKS "" 1
+venv/bin/python ./cli.py 5 8 $LAST_N_WEEKS "" 1
 cp out/flow/csv/latest.csv out/flow.csv
-
 # Psychology
 venv/bin/python ./cli.py 18 8 $LAST_N_WEEKS "" 1
 cp out/psychology/csv/latest.csv out/psychology.csv
--- xWeekAgo.sh
+++ xWeekAgo.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-
-X_WEEK_AGO=${1:-1}
+LAST_N_WEEKS=${1:-1}
 
 # PhD
-venv/bin/python ./cli.py 1 9 $X_WEEK_AGO "" 1
+venv/bin/python ./cli.py 9 9 $LAST_N_WEEKS "" 1
 cp out/phd/csv/latest.csv out/phd.csv
 # stasbar
-venv/bin/python ./cli.py 2 9 $X_WEEK_AGO "" 1
+venv/bin/python ./cli.py 15 9 $LAST_N_WEEKS "" 1
 cp out/stasbar-sp-z-o-o/csv/latest.csv out/stasbar.csv
 # Dancing
-venv/bin/python ./cli.py 16 9 $X_WEEK_AGO "" 1
+venv/bin/python ./cli.py 4 9 $LAST_N_WEEKS "" 1
 cp out/dancing/csv/latest.csv out/dancing.csv
 # Friends
-venv/bin/python ./cli.py 15 9 $X_WEEK_AGO "" 1
+venv/bin/python ./cli.py 7 9 $LAST_N_WEEKS "" 1
 cp out/friends/csv/latest.csv out/friends.csv
 # Self-investement
-venv/bin/python ./cli.py 12 9 $X_WEEK_AGO "" 1
+venv/bin/python ./cli.py 8 9 $LAST_N_WEEKS "" 1
 cp out/self-investment/csv/latest.csv out/self-investment.csv
 # Health
-venv/bin/python ./cli.py 14 9 $X_WEEK_AGO "" 1
+venv/bin/python ./cli.py 3 9 $LAST_N_WEEKS "" 1
 cp out/health/csv/latest.csv out/health.csv
 # Flow
-venv/bin/python ./cli.py 17 9 $X_WEEK_AGO "" 1
+venv/bin/python ./cli.py 5 9 $LAST_N_WEEKS "" 1
 cp out/flow/csv/latest.csv out/flow.csv
 # Psychology
-venv/bin/python ./cli.py 18 9 $X_WEEK_AGO "" 1
+venv/bin/python ./cli.py 18 9 $LAST_N_WEEKS "" 1
 cp out/psychology/csv/latest.csv out/psychology.csv
